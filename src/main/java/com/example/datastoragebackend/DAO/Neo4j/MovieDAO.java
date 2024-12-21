c614b5d54156cfdae6abd53eb84036d8b8686fe2
王智 <1848195475@qq.com>
2024-12-21 15:55:55 +0800

    neo4j try

---
+++ src/main/java/com/example/datastoragebackend/DAO/Neo4j/MovieDAO.java
@@ -0,0 +1,13 @@
+package com.example.datastoragebackend.DAO.Neo4j;
+
+import com.example.datastoragebackend.Entity.Neo4j.Movie;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface MovieDAO extends Neo4jRepository<Movie,String> {
+    List<Movie> findByTitle(String title);
+}
---
+++ src/main/java/com/example/datastoragebackend/Entity/Neo4j/Movie.java
@@ -0,0 +1,20 @@
+package com.example.datastoragebackend.Entity.Neo4j;
+
+import lombok.Data;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+
+@Node("Movie")
+@Data
+public class Movie {
+    @Id
+    private String id;
+
+    private String title;
+
+    private String genre;
+
+    private String releaseDate;
+
+    private String score;
+}
