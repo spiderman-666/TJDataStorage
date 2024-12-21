49cd7072250d02ee5997e1defd16d4698a893610
王智 <1848195475@qq.com>
2024-12-21 10:01:48 +0800

    fix

--- src/main/java/com/example/datastoragebackend/Controller/QueryController.java
+++ src/main/java/com/example/datastoragebackend/Controller/QueryController.java
@@ -6,10 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.datastoragebackend.Service.QueryService;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+
+import java.util.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
@@ -19,58 +17,55 @@ public class QueryController {
     private MovieService movieService;
 
     @PostMapping("/query")
-    public ResponseEntity<Map<String, Object>>  handleQuery(@RequestBody Map<String, Object> query) {
-        String queryType = (String) query.get("type");
-        Map<String, Object> result= new HashMap<>();
+    public ResponseEntity<List<Map<String, String>>>  handleQuery(@RequestBody Map<String, String> query) {
+        String queryType = query.get("type");
+        List<Map<String, String>> result= new ArrayList<>();
         try {
             switch (queryType) {
                 case "time":
-                    String startDate = (String) query.get("startDate");
-                    String endDate = (String) query.get("endDate");
+                    String startDate = query.get("startDate");
+                    String endDate = query.get("endDate");
                     //result = queryByTime(startDate, endDate);
                     break;
                 case "name":
-                    String movieName = (String) query.get("movieName");
+                    String movieName = query.get("movieName");
                     // 查询电影信息
-                    List<Movie> movies = movieService.getMoviesByTitle(movieName);
-                    System.out.println("查询到的电影数据: " + movies);  // 确认是否查询到正确的电影数据
-                    result.put("movies", movies);
+                    result = movieService.getMoviesByTitle(movieName);
+                    System.out.println("查询到的电影数据: " + result);  // 确认是否查询到正确的电影数据
                     break;
                 case "director":
-                    String directorName = (String) query.get("directorName");
+                    String directorName = query.get("directorName");
                     //result = queryByDirector(directorName);
                     break;
                 case "actor":
-                    String actorName = (String) query.get("actorName");
+                    String actorName = query.get("actorName");
                     //result = queryByActor(actorName);
                     break;
                 case "relationship":
-                    String director = (String) query.get("directorName");
-                    String actor = (String) query.get("actorName");
+                    String director = query.get("directorName");
+                    String actor = query.get("actorName");
                     //result = queryByRelationship(director, actor);
                     break;
                 case "category":
-                    String categoryName = (String) query.get("categoryName");
+                    String categoryName = query.get("categoryName");
                     //result = queryByCategory(categoryName);
                     break;
                 case "rating":
-                    Integer minRating = (Integer) query.get("minRating");
-                    Integer maxRating = (Integer) query.get("maxRating");
+                    Integer minRating = Integer.parseInt(query.get("minRating"));
+                    Integer maxRating = Integer.parseInt(query.get("maxRating"));
                     //result = queryByRating(minRating, maxRating);
                     break;
                 case "combination":
-                    String keyword = (String) query.get("keyword");
+                    String keyword = query.get("keyword");
                     //result = queryByCombination(keyword);
                     break;
                 default:
                     throw new IllegalArgumentException("未知的查询类型: " + queryType);
             }
-            result.put("message","查找成功");
             System.out.println(result);
             return ResponseEntity.ok(result);
 
         } catch (Exception e) {
-            result.put("message", "查找失败");
             return ResponseEntity.status(500).body(result);
         }
 
--- src/main/java/com/example/datastoragebackend/Service/MovieService.java
+++ src/main/java/com/example/datastoragebackend/Service/MovieService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.datastoragebackend.DAO.MovieDAO;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,8 +17,22 @@ public class MovieService {
 
     // 根据电影名查询电影
     // 根据电影名查询所有电影信息
-    public List<Movie> getMoviesByTitle(String movieName) {
-        return movieDAO.findByTitle(movieName);
+    public List<Map<String, String>> getMoviesByTitle(String movieName) {
+        List<Movie> movies = movieDAO.findByTitle(movieName);
+        List<Map<String, String>> result = new ArrayList<>();
+        for (Movie movie : movies) {
+            Map<String, String> map = new HashMap<>();
+            map.put("id", movie.getID());
+            map.put("title", movie.getTitle());
+            map.put("duration", movie.getDuration());
+            map.put("score", movie.getScore());
+            map.put("releaseDate", movie.getReleaseDate());
+            map.put("genre", movie.getGenre());
+            map.put("reviewCount", String.valueOf(movie.getReviewCount()));
+            map.put("source_type", movie.getSource_type());
+            result.add(map);
+        }
+        return result;
     }
 
 
